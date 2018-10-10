2bcbfadf35dd4c4e9082abc2d4eb45bb19d04d7e
tackyunicorn <joshjoseph97@gmail.com>
2018-10-10 13:26:50 +0530

    updated 4

--- 4/4.c
+++ 4/4.c
@@ -4,74 +4,96 @@
 int i=0;
 char ip[20];
 
-void S();
-void A();
-void B();
+int S();
+int A();
+int B();
 
 void main()
 {
-        char ip[20];
         scanf("\n%s" , ip);
-        S();
-        printf("\nEnd of RD Parser\n");
+	printf("\n");
+        if(S())
+        	printf("\nValid expression!\n");
+	else
+		printf("\nNot a valid expression!\n");
 }
 
-void S()
+int S()
 {
 	if(ip[i] == 'c' && ip[i+1] == 'd')
 	{
-		printf("cd match: S-->cd\n");
+		printf("S-->cd\n");
 		i += 2;
-	}
-	else if(ip[i] == 'a' || ip[i] == 'b')
-	{
-		A();
-		B();
-		printf("S-->AB\n");
+		return 1;
 	}
 	else if(ip[i] == 'c' || ip[i] == 'b')
+        {
+                if(B())
+                {
+                        printf("S-->B\n");
+                        return 1;
+                }
+        }
+	else if(ip[i] == 'a' || ip[i] == 'b')
 	{
-		B();
-		printf("S-->B\n");
-	}
-	else
-		printf("invalid\n");
-}
-
-void A()
-{
-	if(ip[i] == 'a')
-	{
-		printf("a match: A-->a\n");
-		i++;
-	}
-	else if(ip[i] == 'b')
-	{
-		printf("b match: A-->b\n");
-		i++;
-	}
-	else
-		printf("invalid\n");
-}
-
-void B()
-{
-	if(ip[i] == 'b')
-	{
-		printf("b match: B-->b\n");
-		i++;
-	}
-	else if(ip[i] == 'c')
-	{
-		printf("c match\n");
-		i++;
-		A();
-		if(ip[i] == 'd')
+		if(A() && B())
 		{
-			printf("cAd match: S-->cAd\n");
-			i++;
+			printf("S-->AB\n");
+			return 1;
 		}
 	}
 	else
+	{
 		printf("invalid\n");
+		return 0;
+	}
+}
+
+int A()
+{
+	if(ip[i] == 'a')
+	{
+		printf("A-->a\n");
+		i++;
+		return 1;
+	}
+	else if(ip[i] == 'b')
+	{
+		printf("A-->b\n");
+		i++;
+		return 1;
+	}
+	else
+	{
+		printf("invalid\n");
+		return 0;
+	}
+}
+
+int B()
+{
+	if(ip[i] == 'b')
+	{
+		printf("B-->b\n");
+		i++;
+		return 1;
+	}
+	else if(ip[i] == 'c')
+	{
+		i++;
+		if(A())
+		{
+			if(ip[i] == 'd')
+			{
+				printf("B-->cAd\n");
+				i++;
+				return 1;
+			}
+		}
+	}
+	else
+	{
+		printf("invalid\n");
+		return 0;
+	}
 }

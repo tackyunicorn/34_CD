656a8fe90b6c0e4e216f2572736ccf710a57f77a
tackyunicorn <joshjoseph97@gmail.com>
2018-11-17 21:07:34 +0530

    corrected 6.c

--- 6/6.c
+++ 6/6.c
@@ -3,14 +3,14 @@
 #include<string.h>
 
 char x , a , ret , stack[20] , ip[20] , cell[4];
-int i , j , ptr=0 ,top=-1 , max=20;
+int i , j , ptr=0 ,top=-1 , max=20, flag=0;
 char table[6][7][4] = {
-			"NT", "I",  "+",    "*",    "(",   ")",  "$",
+			"NT", "i",  "+",    "*",    "(",   ")",  "$",
 			"E",  "TX", "-",    "-",    "TX", "-",  "-",
 			"X", "-",   "+TX", "-",    "-",   "\0", "\0",
 			"T",  "FY", "-",    "-",    "FY", "-",  "-",
 			"Y", "-",   "\0",   "*FY", "-",   "\0", "\0",
-			"F",  "I",  "-",    "-",    "(E)", "-",  "-",
+			"F",  "i",  "-",    "-",    "(E)", "-",  "-",
 			};
 
 void push(char c)
@@ -31,6 +31,7 @@ void main()
 	printf("Enter the string: ");
 	scanf("%s" , ip);
 
+	printf("\nThe table is: \n");
 	for(i=0 ; i<6 ; i++)
 	{
 		for(j=0 ; j<7 ; j++)
@@ -42,19 +43,28 @@ void main()
 	push('E');
 	while(top != -1)
 	{
+		printf("\n\nSTACK: \n");
+		for(i=top ; i>=0 ; i--)
+			printf("%c\n" , stack[i]);
+
 		x = pop();
 		a = ip[ptr];
+
 		if(!isupper(x))
 			if(x == a)
 				ptr += 1;
 			else
-				printf("[1]Invalid input!\n");
+			{
+				flag = 1;
+				break;
+			}
 		else
 		{
 			for(i=1 ; i<6 ; i++)
 				if(x == table[i][0][0])
 					break;
-			if(islower(a))
+
+			if(isalpha(a) && islower(a))
 				j=1;
 			else
 			{
@@ -65,23 +75,25 @@ void main()
 
 			strcpy(cell , table[i][j]);
 
-			if(!strcmp(cell , "-"))
-				printf("[2]Invalid input!\n");
+			if(cell[0] == '-')
+			{
+				flag = 1;
+				break;
+			}
 			else if(strcmp(cell , "\0"))
 			{
-				for(i=strlen(cell)-1 ; i>=0 ; i--)
-					push(cell[i]);
+				if(cell[0] == 'i')
+					push(a);
+				else
+					for(i=strlen(cell)-1 ; i>=0 ; i--)
+						push(cell[i]);
 			}
 
 		}
-
-		printf("\n\n");
-		for(i=top ; i>=0 ; i--)
-			printf("%c\n" , stack[top]);
 	}
 
-	if(ptr == strlen(ip)+1)
+	if((ip[ptr] == '\0') && (flag == 0))
 		printf("Valid input!\n");
 	else
-		printf("[3]Invalid input!\n");
+		printf("Invalid input!\n");
 }
